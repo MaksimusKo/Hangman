05b8eb3bd3079726d04162f6bae9dc18bb454131
MaksimusKo <121377713+MaksimusKo@users.noreply.github.com>
2023-03-23 22:00:20 +0900

    Lol

--- Hangman.py
+++ Hangman.py
@@ -2,7 +2,7 @@ import random
 print('Это словесная угадайка, у вас есть 6 попыток что бы угадать слово')
 
 #words = ['WHAT','секретарь','знакомый','войско','звезда','двор','бутылка','чувство','этап','сердце','честь']
-words = ['what']
+words = ['OBNOXIOUS','DEATH','HERO','TWENTY','THIRTY','FORTY','SIXTY']
 ranwords = random.choice(words).lower()
 Hangman1 = (
     """
@@ -70,6 +70,9 @@ def play():
     guessed = []
     wrong = []
     count = 1
+    hintcount = 0
+    print('Подсказка, в слове', len(ranwords), 'буквы')
+    print('Если вы хотите ещё подсказку нажмите 1')
     while count <= 6:
         guess = input()
         if count == 6:
@@ -80,15 +83,18 @@ def play():
             print('Вы уже вводили эту букву')
         if len(guess) > 1 and guess != ranwords:
             print('введите одну букву')
+        if guess in '234567890':
+            print('Введите букву')
         elif guess in ranwords:
             if len(guess) == 1:
                 print('буква', guess, 'есть в слове')
                 guessed.append(guess)
                 print('ваши угаданные буквы', guessed, )
+                print('буква',guess,ranwords.index(guess)+1,'буква в слове')
         if guess == ranwords:
             print('Вы победили')
             exit()
-        if guess != ranwords and guess not in ranwords and guess not in wrong and len(guess) == 1:
+        if guess != ranwords and guess not in ranwords and guess not in wrong and len(guess) == 1 and not guess in '1234567890':
             print('Неправильный ввод')
             print('ваши угаданные буквы', guessed, )
             count += 1
@@ -103,6 +109,12 @@ def play():
                 print(Hangman3)
             elif count == 5:
                 print(Hangman2)
+        if guess == '1':
+            if hintcount < len(ranwords) / 2:
+                hintcount += 1
+                print('в слове есть буква', random.choice(ranwords))
+            else:
+                print('Подсказкой больше нельзя воспользоватся')
 
 
 play()
