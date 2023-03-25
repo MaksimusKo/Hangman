34f1ba56a40dca09b9399f07861fcf4c06c93278
MaksimusKo <121377713+MaksimusKo@users.noreply.github.com>
2023-03-25 19:24:37 +0900

    Hangman logic changed to dict

--- Hangman.py
+++ Hangman.py
@@ -3,70 +3,19 @@ print('Это словесная угадайка, у вас есть 6 попы
 
 #words = ['WHAT','секретарь','знакомый','войско','звезда','двор','бутылка','чувство','этап','сердце','честь']
 words = ['OBNOXIOUS','DEATH','HERO','TWENTY','THIRTY','FORTY','SIXTY']
-# words = ['wtf']
+# words = ['BARABARABARA']
 ranwords = random.choice(words).lower()
-Hangman1 = (
-    """
-     ------
-     |    |
-     |    O
-     |   /|\\
-     |   / \\
-     |   
-     |   
-    ----------
-    """
-)
-Hangman2= (    """
-     ------
-     |    |
-     |    O
-     |   /|\\
-     |   / 
-     |   
-     |   
-    ----------
-    """)
-Hangman3=(    """
-     ------
-     |    |
-     |    O
-     |   /|\\
-     |   
-     |   
-     |   
-    ----------
-    """)
-Hangman4= (    """
-     ------
-     |    |
-     |    O
-     | 
-     |  
-     |   
-     |   
-    ----------
-    """)
-Hangman5= (    """
-     ------
-     |    |
-     |    
-     |  
-     |   
-     |   
-     |   
-    ----------
-    """)
-Hangman6= (    """
-     ------
-     |    
-     |   
-     |   
-     |   
-     |   
-     |   
-    ----------
-    """)
+hangman_dict = {
+    0: '',
+    1: '__________\n|         |\n|\n|\n|\n|',
+    2: '__________\n|         |\n|         O\n|\n|\n|',
+    3: '__________\n|         |\n|         O\n|         |\n|\n|',
+    4: '__________\n|         |\n|         O\n|        /|\n|\n|',
+    5: '__________\n|         |\n|         O\n|        /|\\\n|\n|',
+    6: '__________\n|         |\n|         O\n|        /|\\\n|        /\n|',
+    7: '__________\n|         |\n|         O\n|        /|\\\n|        / \\\n|'
+}
+
 def play():
     guessed = []
     wrong = []
@@ -80,7 +29,7 @@ def play():
         guess = input()
         if count == 6:
             print('Вы проиграли')
-            print(Hangman1)
+            print(hangman_dict.get(7))
             exit()
         if guess in wrong or guess in guessed:
             print('Вы уже вводили эту букву')
@@ -90,11 +39,13 @@ def play():
             print('Введите букву')
         elif guess in ranwords:
             if len(guess) == 1:
-                print('буква', guess, 'есть в слове')
+                print('буква(ы)', guess, 'есть в слове')
                 guessed.append(guess)
-                wordds[ranwords.index(guess)] = guess
-                print('ваше слово',''.join(wordds))
-        if guess == ranwords:
+                for i in range(len(ranwords)):
+                    if ranwords[i] == guess:
+                        wordds[i] = guess
+                print('ваше слово', ''.join(wordds))
+        if guess == ranwords or ''.join(wordds) == ranwords:
             print('Вы победили')
             exit()
         if guess != ranwords and guess not in ranwords and guess not in wrong and len(guess) == 1 and not guess in '1234567890':
@@ -102,16 +53,8 @@ def play():
             print('ваше слово', ''.join(wordds))
             count += 1
             wrong.append(guess)
-            if count == 1:
-                print(Hangman6)
-            elif count == 2:
-                print(Hangman5)
-            elif count == 3:
-                print(Hangman4)
-            elif count == 4:
-                print(Hangman3)
-            elif count == 5:
-                print(Hangman2)
+            if count in hangman_dict:
+                print(hangman_dict[count]) #сама виселица
         if guess == '1':
             if hintcount < len(ranwords) / 2:
                 hintcount += 1
